11ad415b6ca41ef21d414daf394fbf7fd2efc435
knox <nikita.3.khorev@gmail.com>
2024-05-25 20:15:25 +0300

    Добавил каждому элементу формы системное имя.

--- 2lab/2lab.h
+++ 2lab/2lab.h
@@ -35,26 +35,44 @@ namespace u2lab {
 			}
 		}
 	private: System::Windows::Forms::Button^ start_btn;
+	private: System::Windows::Forms::GroupBox^ array_groupBox;
+
+	private: System::Windows::Forms::GroupBox^ sorting_groupBox;
+	private: System::Windows::Forms::GroupBox^ data_groupBox;
 	protected:
 
 	protected:
 
-	private: System::Windows::Forms::GroupBox^ groupBox1;
-	private: System::Windows::Forms::GroupBox^ groupBox2;
-	private: System::Windows::Forms::GroupBox^ groupBox3;
-	private: System::Windows::Forms::Button^ button2;
+
+
+
+	private: System::Windows::Forms::Button^ reset_btn;
+
+
 	private: System::Windows::Forms::Button^ info_btn;
+	private: System::Windows::Forms::Button^ err_btn;
+	private: System::Windows::Forms::GroupBox^ result_groupBox;
+	private: System::Windows::Forms::RadioButton^ RadixSort;
+	private: System::Windows::Forms::RadioButton^ InsertionSort;
 
-	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::GroupBox^ groupBox4;
-	private: System::Windows::Forms::RadioButton^ radioButton1;
-	private: System::Windows::Forms::RadioButton^ radioButton2;
-	private: System::Windows::Forms::RadioButton^ radioButton3;
-	private: System::Windows::Forms::RadioButton^ radioButton4;
-	private: System::Windows::Forms::RadioButton^ radioButton5;
-	private: System::Windows::Forms::RadioButton^ radioButton7;
-	private: System::Windows::Forms::RadioButton^ radioButton6;
-	private: System::Windows::Forms::RadioButton^ radioButton8;
+
+
+
+
+
+	private: System::Windows::Forms::RadioButton^ BubbleSort;
+	private: System::Windows::Forms::RadioButton^ CocktailSort;
+
+
+
+	private: System::Windows::Forms::RadioButton^ QuickSort;
+
+	private: System::Windows::Forms::RadioButton^ arr2;
+
+	private: System::Windows::Forms::RadioButton^ arr1;
+	private: System::Windows::Forms::RadioButton^ arr3;
+
+
 
 
 
@@ -71,25 +89,24 @@ namespace u2lab {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(u2lab::typeid));
 			this->start_btn = (gcnew System::Windows::Forms::Button());
-			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->radioButton8 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton7 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton6 = (gcnew System::Windows::Forms::RadioButton());
-			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
-			this->radioButton5 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton4 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
-			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->array_groupBox = (gcnew System::Windows::Forms::GroupBox());
+			this->arr3 = (gcnew System::Windows::Forms::RadioButton());
+			this->arr2 = (gcnew System::Windows::Forms::RadioButton());
+			this->arr1 = (gcnew System::Windows::Forms::RadioButton());
+			this->sorting_groupBox = (gcnew System::Windows::Forms::GroupBox());
+			this->QuickSort = (gcnew System::Windows::Forms::RadioButton());
+			this->RadixSort = (gcnew System::Windows::Forms::RadioButton());
+			this->CocktailSort = (gcnew System::Windows::Forms::RadioButton());
+			this->InsertionSort = (gcnew System::Windows::Forms::RadioButton());
+			this->BubbleSort = (gcnew System::Windows::Forms::RadioButton());
+			this->data_groupBox = (gcnew System::Windows::Forms::GroupBox());
+			this->reset_btn = (gcnew System::Windows::Forms::Button());
 			this->info_btn = (gcnew System::Windows::Forms::Button());
-			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
-			this->groupBox1->SuspendLayout();
-			this->groupBox2->SuspendLayout();
+			this->err_btn = (gcnew System::Windows::Forms::Button());
+			this->result_groupBox = (gcnew System::Windows::Forms::GroupBox());
+			this->array_groupBox->SuspendLayout();
+			this->sorting_groupBox->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// start_btn
@@ -102,137 +119,132 @@ namespace u2lab {
 			this->start_btn->UseVisualStyleBackColor = true;
 			this->start_btn->Click += gcnew System::EventHandler(this, &u2lab::start_btn_Click);
 			// 
-			// groupBox1
+			// array_groupBox
 			// 
-			this->groupBox1->Controls->Add(this->radioButton8);
-			this->groupBox1->Controls->Add(this->radioButton7);
-			this->groupBox1->Controls->Add(this->radioButton6);
-			this->groupBox1->Location = System::Drawing::Point(12, 12);
-			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(191, 119);
-			this->groupBox1->TabIndex = 2;
-			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Варианты массивов";
+			this->array_groupBox->Controls->Add(this->arr3);
+			this->array_groupBox->Controls->Add(this->arr2);
+			this->array_groupBox->Controls->Add(this->arr1);
+			this->array_groupBox->Location = System::Drawing::Point(12, 12);
+			this->array_groupBox->Name = L"array_groupBox";
+			this->array_groupBox->Size = System::Drawing::Size(191, 119);
+			this->array_groupBox->TabIndex = 2;
+			this->array_groupBox->TabStop = false;
+			this->array_groupBox->Text = L"Варианты массивов";
 			// 
-			// radioButton8
+			// arr3
 			// 
-			this->radioButton8->AutoSize = true;
-			this->radioButton8->Location = System::Drawing::Point(6, 85);
-			this->radioButton8->Name = L"radioButton8";
-			this->radioButton8->Size = System::Drawing::Size(103, 24);
-			this->radioButton8->TabIndex = 8;
-			this->radioButton8->TabStop = true;
-			this->radioButton8->Text = L"Массив 3";
-			this->radioButton8->UseVisualStyleBackColor = true;
+			this->arr3->AutoSize = true;
+			this->arr3->Location = System::Drawing::Point(6, 85);
+			this->arr3->Name = L"arr3";
+			this->arr3->Size = System::Drawing::Size(103, 24);
+			this->arr3->TabIndex = 8;
+			this->arr3->TabStop = true;
+			this->arr3->Text = L"Массив 3";
+			this->arr3->UseVisualStyleBackColor = true;
 			// 
-			// radioButton7
+			// arr2
 			// 
-			this->radioButton7->AutoSize = true;
-			this->radioButton7->Location = System::Drawing::Point(6, 55);
-			this->radioButton7->Name = L"radioButton7";
-			this->radioButton7->Size = System::Drawing::Size(103, 24);
-			this->radioButton7->TabIndex = 7;
-			this->radioButton7->TabStop = true;
-			this->radioButton7->Text = L"Массив 2";
-			this->radioButton7->UseVisualStyleBackColor = true;
+			this->arr2->AutoSize = true;
+			this->arr2->Location = System::Drawing::Point(6, 55);
+			this->arr2->Name = L"arr2";
+			this->arr2->Size = System::Drawing::Size(103, 24);
+			this->arr2->TabIndex = 7;
+			this->arr2->TabStop = true;
+			this->arr2->Text = L"Массив 2";
+			this->arr2->UseVisualStyleBackColor = true;
 			// 
-			// radioButton6
+			// arr1
 			// 
-			this->radioButton6->AutoSize = true;
-			this->radioButton6->Location = System::Drawing::Point(6, 25);
-			this->radioButton6->Name = L"radioButton6";
-			this->radioButton6->Size = System::Drawing::Size(103, 24);
-			this->radioButton6->TabIndex = 6;
-			this->radioButton6->TabStop = true;
-			this->radioButton6->Text = L"Массив 1";
-			this->radioButton6->UseVisualStyleBackColor = true;
+			this->arr1->AutoSize = true;
+			this->arr1->Location = System::Drawing::Point(6, 25);
+			this->arr1->Name = L"arr1";
+			this->arr1->Size = System::Drawing::Size(103, 24);
+			this->arr1->TabIndex = 6;
+			this->arr1->TabStop = true;
+			this->arr1->Text = L"Массив 1";
+			this->arr1->UseVisualStyleBackColor = true;
 			// 
-			// groupBox2
+			// sorting_groupBox
 			// 
-			this->groupBox2->Controls->Add(this->radioButton5);
-			this->groupBox2->Controls->Add(this->radioButton1);
-			this->groupBox2->Controls->Add(this->radioButton4);
-			this->groupBox2->Controls->Add(this->radioButton2);
-			this->groupBox2->Controls->Add(this->radioButton3);
-			this->groupBox2->Location = System::Drawing::Point(12, 137);
-			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(191, 176);
-			this->groupBox2->TabIndex = 0;
-			this->groupBox2->TabStop = false;
-			this->groupBox2->Text = L"Варианты сортировок";
+			this->sorting_groupBox->Controls->Add(this->QuickSort);
+			this->sorting_groupBox->Controls->Add(this->RadixSort);
+			this->sorting_groupBox->Controls->Add(this->CocktailSort);
+			this->sorting_groupBox->Controls->Add(this->InsertionSort);
+			this->sorting_groupBox->Controls->Add(this->BubbleSort);
+			this->sorting_groupBox->Location = System::Drawing::Point(12, 137);
+			this->sorting_groupBox->Name = L"sorting_groupBox";
+			this->sorting_groupBox->Size = System::Drawing::Size(191, 176);
+			this->sorting_groupBox->TabIndex = 0;
+			this->sorting_groupBox->TabStop = false;
+			this->sorting_groupBox->Text = L"Варианты сортировок";
 			// 
-			// radioButton5
+			// QuickSort
 			// 
-			this->radioButton5->AutoSize = true;
-			this->radioButton5->Location = System::Drawing::Point(6, 145);
-			this->radioButton5->Name = L"radioButton5";
-			this->radioButton5->Size = System::Drawing::Size(100, 24);
-			this->radioButton5->TabIndex = 10;
-			this->radioButton5->TabStop = true;
-			this->radioButton5->Text = L"Быстрая";
-			this->radioButton5->UseVisualStyleBackColor = true;
+			this->QuickSort->AutoSize = true;
+			this->QuickSort->Location = System::Drawing::Point(6, 145);
+			this->QuickSort->Name = L"QuickSort";
+			this->QuickSort->Size = System::Drawing::Size(100, 24);
+			this->QuickSort->TabIndex = 10;
+			this->QuickSort->Text = L"Быстрая";
+			this->QuickSort->UseVisualStyleBackColor = true;
 			// 
-			// radioButton1
+			// RadixSort
 			// 
-			this->radioButton1->AutoSize = true;
-			this->radioButton1->Location = System::Drawing::Point(6, 25);
-			this->radioButton1->Name = L"radioButton1";
-			this->radioButton1->Size = System::Drawing::Size(137, 24);
-			this->radioButton1->TabIndex = 6;
-			this->radioButton1->TabStop = true;
-			this->radioButton1->Text = L"Поразрядная";
-			this->radioButton1->UseVisualStyleBackColor = true;
+			this->RadixSort->AutoSize = true;
+			this->RadixSort->Location = System::Drawing::Point(6, 25);
+			this->RadixSort->Name = L"RadixSort";
+			this->RadixSort->Size = System::Drawing::Size(137, 24);
+			this->RadixSort->TabIndex = 6;
+			this->RadixSort->Text = L"Поразрядная";
+			this->RadixSort->UseVisualStyleBackColor = true;
 			// 
-			// radioButton4
+			// CocktailSort
 			// 
-			this->radioButton4->AutoSize = true;
-			this->radioButton4->Location = System::Drawing::Point(6, 115);
-			this->radioButton4->Name = L"radioButton4";
-			this->radioButton4->Size = System::Drawing::Size(100, 24);
-			this->radioButton4->TabIndex = 9;
-			this->radioButton4->TabStop = true;
-			this->radioButton4->Text = L"Шейкера";
-			this->radioButton4->UseVisualStyleBackColor = true;
+			this->CocktailSort->AutoSize = true;
+			this->CocktailSort->Location = System::Drawing::Point(6, 115);
+			this->CocktailSort->Name = L"CocktailSort";
+			this->CocktailSort->Size = System::Drawing::Size(100, 24);
+			this->CocktailSort->TabIndex = 9;
+			this->CocktailSort->Text = L"Шейкера";
+			this->CocktailSort->UseVisualStyleBackColor = true;
 			// 
-			// radioButton2
+			// InsertionSort
 			// 
-			this->radioButton2->AutoSize = true;
-			this->radioButton2->Location = System::Drawing::Point(6, 55);
-			this->radioButton2->Name = L"radioButton2";
-			this->radioButton2->Size = System::Drawing::Size(117, 24);
-			this->radioButton2->TabIndex = 7;
-			this->radioButton2->TabStop = true;
-			this->radioButton2->Text = L"Вставками";
-			this->radioButton2->UseVisualStyleBackColor = true;
+			this->InsertionSort->AutoSize = true;
+			this->InsertionSort->Location = System::Drawing::Point(6, 55);
+			this->InsertionSort->Name = L"InsertionSort";
+			this->InsertionSort->Size = System::Drawing::Size(117, 24);
+			this->InsertionSort->TabIndex = 7;
+			this->InsertionSort->Text = L"Вставками";
+			this->InsertionSort->UseVisualStyleBackColor = true;
 			// 
-			// radioButton3
+			// BubbleSort
 			// 
-			this->radioButton3->AutoSize = true;
-			this->radioButton3->Location = System::Drawing::Point(6, 85);
-			this->radioButton3->Name = L"radioButton3";
-			this->radioButton3->Size = System::Drawing::Size(134, 24);
-			this->radioButton3->TabIndex = 8;
-			this->radioButton3->TabStop = true;
-			this->radioButton3->Text = L"Пузырьковая";
-			this->radioButton3->UseVisualStyleBackColor = true;
+			this->BubbleSort->AutoSize = true;
+			this->BubbleSort->Location = System::Drawing::Point(6, 85);
+			this->BubbleSort->Name = L"BubbleSort";
+			this->BubbleSort->Size = System::Drawing::Size(134, 24);
+			this->BubbleSort->TabIndex = 8;
+			this->BubbleSort->Text = L"Пузырьковая";
+			this->BubbleSort->UseVisualStyleBackColor = true;
 			// 
-			// groupBox3
+			// data_groupBox
 			// 
-			this->groupBox3->Location = System::Drawing::Point(209, 12);
-			this->groupBox3->Name = L"groupBox3";
-			this->groupBox3->Size = System::Drawing::Size(200, 429);
-			this->groupBox3->TabIndex = 0;
-			this->groupBox3->TabStop = false;
-			this->groupBox3->Text = L"Данные";
+			this->data_groupBox->Location = System::Drawing::Point(209, 12);
+			this->data_groupBox->Name = L"data_groupBox";
+			this->data_groupBox->Size = System::Drawing::Size(200, 429);
+			this->data_groupBox->TabIndex = 0;
+			this->data_groupBox->TabStop = false;
+			this->data_groupBox->Text = L"Данные";
 			// 
-			// button2
+			// reset_btn
 			// 
-			this->button2->Location = System::Drawing::Point(12, 319);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(77, 36);
-			this->button2->TabIndex = 3;
-			this->button2->Text = L"Сброс";
-			this->button2->UseVisualStyleBackColor = true;
+			this->reset_btn->Location = System::Drawing::Point(12, 319);
+			this->reset_btn->Name = L"reset_btn";
+			this->reset_btn->Size = System::Drawing::Size(77, 36);
+			this->reset_btn->TabIndex = 3;
+			this->reset_btn->Text = L"Сброс";
+			this->reset_btn->UseVisualStyleBackColor = true;
 			// 
 			// info_btn
 			// 
@@ -244,45 +256,45 @@ namespace u2lab {
 			this->info_btn->UseVisualStyleBackColor = true;
 			this->info_btn->Click += gcnew System::EventHandler(this, &u2lab::info_btn_Click);
 			// 
-			// button4
+			// err_btn
 			// 
-			this->button4->Location = System::Drawing::Point(12, 401);
-			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(191, 40);
-			this->button4->TabIndex = 5;
-			this->button4->Text = L"Проверка на ошибки";
-			this->button4->UseVisualStyleBackColor = true;
+			this->err_btn->Location = System::Drawing::Point(12, 401);
+			this->err_btn->Name = L"err_btn";
+			this->err_btn->Size = System::Drawing::Size(191, 40);
+			this->err_btn->TabIndex = 5;
+			this->err_btn->Text = L"Проверка на ошибки";
+			this->err_btn->UseVisualStyleBackColor = true;
 			// 
-			// groupBox4
+			// result_groupBox
 			// 
-			this->groupBox4->Location = System::Drawing::Point(415, 12);
-			this->groupBox4->Name = L"groupBox4";
-			this->groupBox4->Size = System::Drawing::Size(200, 429);
-			this->groupBox4->TabIndex = 0;
-			this->groupBox4->TabStop = false;
-			this->groupBox4->Text = L"Результат";
+			this->result_groupBox->Location = System::Drawing::Point(415, 12);
+			this->result_groupBox->Name = L"result_groupBox";
+			this->result_groupBox->Size = System::Drawing::Size(200, 429);
+			this->result_groupBox->TabIndex = 0;
+			this->result_groupBox->TabStop = false;
+			this->result_groupBox->Text = L"Результат";
 			// 
 			// u2lab
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(629, 458);
-			this->Controls->Add(this->groupBox4);
-			this->Controls->Add(this->button4);
+			this->Controls->Add(this->result_groupBox);
+			this->Controls->Add(this->err_btn);
 			this->Controls->Add(this->info_btn);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->groupBox2);
-			this->Controls->Add(this->groupBox3);
-			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->reset_btn);
+			this->Controls->Add(this->sorting_groupBox);
+			this->Controls->Add(this->data_groupBox);
+			this->Controls->Add(this->array_groupBox);
 			this->Controls->Add(this->start_btn);
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"u2lab";
 			this->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->ShowIcon = false;
 			this->Text = L"Lab 2 - Sorting arrays";
-			this->groupBox1->ResumeLayout(false);
-			this->groupBox1->PerformLayout();
-			this->groupBox2->ResumeLayout(false);
-			this->groupBox2->PerformLayout();
+			this->array_groupBox->ResumeLayout(false);
+			this->array_groupBox->PerformLayout();
+			this->sorting_groupBox->ResumeLayout(false);
+			this->sorting_groupBox->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
